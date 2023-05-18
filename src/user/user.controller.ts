0fe871670e560eddc07b47363c8fb3e24f0e88af
mathieu-besson <mathieubesson2001@gmail.com>
2023-05-18 19:15:08 +0200

    ✨ : Fonctionnalité : Création des routes pour la ressource user (controller, module et service)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, ConflictException, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Req } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
+import { ConfirmationActionMessage } from 'src/helpers/root-service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    public constructor(private readonly userService: UserService) { }
+
+    @Get()
+    public readAll() {
+        return this.userService.readAll()
+    }
+
+    @Get(':id')
+    public readOne(@Param('id', ParseIntPipe) userId: number) {
+        return this.userService.readOne(userId)
+    }
+
+    @Post()
+    public async create(@Body() createUserDto: CreateUserDto): Promise<ConfirmationActionMessage> {
+        return await this.userService.create(createUserDto)
+    }
+
+    @Patch(':id')
+    public async patch(@Param('id', ParseIntPipe) userId: number, @Body() updateUserDto: UpdateUserDto): Promise<ConfirmationActionMessage> {
+        return await this.userService.update(userId, updateUserDto)
+    }
+
+    @Delete(':id')
+    public async delete(@Param('id', ParseIntPipe) userId: number): Promise<ConfirmationActionMessage> {
+        return await this.userService.delete(userId)
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
+import { ConfirmationActionMessage, RootService } from 'src/helpers/root-service';
+import { UpdateUserDto } from './dto/update-user-dto';
 
 @Injectable()
-export class UserService {}
+export class UserService extends RootService {
+    public giveEntityName(): string {
+        return "user"
+    }
+
+    public async create(createUserDto: CreateUserDto): Promise<ConfirmationActionMessage> {
+        const { firstName, lastName, email } = createUserDto;
+        // Check if user exist
+        const numberOfUserWithSameEmail = await this.prismaService.user.count({ where: { email } })
+        if (numberOfUserWithSameEmail > 0) {
+            throw new ConflictException(`${this.getEntityNameCapitalized()} with this email already exist`)
+        }
+        // Create user in BDD
+        await this.prismaService.user.create({ data: { firstName, lastName, email } })
+        return { message: `${this.getEntityNameCapitalized()} created` }
+    }
+
+    public async update(id: number, updateEntityDto: UpdateUserDto): Promise<ConfirmationActionMessage> {
+        this.verifyEntityExist(id);
+
+        // Check duplicated email
+        if (updateEntityDto.email) {
+            const numberOfUserWithSameEmail = await this.prismaService.user.count({
+                where: {
+                    email: updateEntityDto.email,
+                    id: { not: id }
+                }
+            })
+            if (numberOfUserWithSameEmail > 0) {
+                throw new ConflictException(`${this.getEntityNameCapitalized()} with this email already exist`)
+            }
+        }
+
+        // Update user
+        await this.prismaService.user.update({ where: { id }, data: { ...updateEntityDto } })
+        return { message: `${this.getEntityNameCapitalized()} updated` }
+    }
+}
